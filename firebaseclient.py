2d7f9b99f4760c8f0f5c0f689b2ef9730040697f
GeorgeLincoln <georgelincolnifce@gmail.com>
2018-10-10 08:36:54 -0300

    Add files via upload

--- firebaseclient.py
+++ firebaseclient.py
@@ -2,12 +2,13 @@ import serial
 import time
 import requests
 import json
+from random import *
 firebase_url = 'https://tvhealth-ifce.firebaseio.com/'
 # Connect to Serial Port for communication
 ser = serial.Serial('COM5', 9600, timeout=0)
 # Setup a loop to send Temperature values at fixed intervals
 # in seconds
-fixed_interval = 10
+fixed_interval = 1
 
 while 1:
     try:
@@ -17,15 +18,17 @@ while 1:
         # current time and date
         time_hhmmss = time.strftime('%H:%M:%S')
         date_mmddyyyy = time.strftime('%d/%m/%Y')
-
+        padrao = "%.2f" % uniform(0, 30)
+        outro = randrange(1000, 2000)
         # current location name
         temperature_location = 'Brasil'
         print(temperature_c + ',' + time_hhmmss + ',' +
               date_mmddyyyy + ',' + temperature_location)
 
         # insert record
-        data = {'date': date_mmddyyyy,
-                'time': time_hhmmss, 'value': temperature_c}
+        data = {'temperatura': temperature_c,
+                'frequencia': padrao,
+                'bpm': outro, }
         result = requests.post(
             firebase_url + '/' + temperature_location + '/temperature.json', data=json.dumps(data))
 
